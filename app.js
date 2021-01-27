62d835a054bf1f600dc64d2c65f65417578cbc91
Sazonov Nikita <actpohabt.ns@gmail.com>
2021-01-27 21:18:55 +0200

    [ ADDED ] basic bot

---
+++ app.js
@@ -0,0 +1,12 @@
+const { Telegraf } = require('telegraf')
+const apiKey = require('./src/tgApiKey')
+
+const bot = new Telegraf(apiKey)
+bot.start((ctx) => ctx.reply('Welcome'))
+bot.help((ctx) => ctx.reply('Send me a sticker'))
+bot.on('sticker', (ctx) => ctx.reply('👍'))
+bot.hears('hi', (ctx) => ctx.reply('Hey there'))
+
+bot.command('review', (ctx) => ctx.reply('Reviewing'))
+
+bot.launch()
